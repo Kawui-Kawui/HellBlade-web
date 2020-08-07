d6d27cc353838e1c6501fb2d6db579ee17590e37
Kawui-Kawui <69362931+Kawui-Kawui@users.noreply.github.com>
2020-08-07 13:04:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.BIEN');
+ScrollReveal().reveal('.REDES');
+ScrollReveal().reveal('.tienda');
+ScrollReveal().reveal('.TC');
+ScrollReveal().reveal('.Tutoriales');
+ScrollReveal().reveal('.Staff-R');
